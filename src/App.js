45ed6fb3584890d79c4c68472453ba9b209d6adc
kaushiki20 <vajpayeekaushiki20@gmail.com>
2020-09-28 16:44:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import DataModal from "./DataModal";
+import axios from "axios";
+import moment from "moment";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [interval, setInterval] = useState([]);
+  const [name, setName] = useState("");
+  const classes = useStyles();
+  useEffect(() => {
+    var config = {
+      method: "get",
+      url: "https://20fbaad1-1df2-4700-9c31-17213065cea7.mock.pstmn.io/api",
+    };
+
+    axios(config)
+      .then(function (response) {
+        const currentData = response.data;
+        console.log(currentData.members);
+        setData(currentData.members);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const handleOpen = index => {
+    setOpen(true);
+    console.log(index);
+    const state = [];
+    for (let i = 0; i < data[index].activity_periods.length; i++) {
+      const start_date = moment(
+        data[index].activity_periods[i].start_time.slice(0, 11)
+      ).format("YYYY-MM-DD");
+      const start_Time = data[index].activity_periods[i].start_time.slice(
+        11,
+        data[index].activity_periods[i].start_time.length
+      );
+
+      const end_date = moment(
+        data[index].activity_periods[i].end_time.slice(0, 11)
+      ).format("YYYY-MM-DD");
+      const end_Time = data[index].activity_periods[i].end_time.slice(
+        11,
+        data[index].activity_periods[i].end_time.length
+      );
+
+      const obj = {
+        start_date: start_date,
+        start_time: start_Time,
+        end_date: end_date,
+        end_time: end_Time,
+      };
+      state.push(obj);
+    }
+    //console.log(state);
+    setInterval(state);
+    setName(data[index].real_name);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setInterval([]);
+    setName();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer className={classes.TableContainer}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableContent} align="right">
+                Id
+              </TableCell>
+              <TableCell className={classes.tableContent} align="right">
+                Name
+              </TableCell>
+              <TableCell className={classes.tableContent} align="right">
+                Time-Zone
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row, index) => (
+              <TableRow key={row.id + index} onClick={() => handleOpen(index)}>
+                <TableCell align="right">{row.id}</TableCell>
+                <TableCell align="right">{row.real_name}</TableCell>
+                <TableCell align="right">{row.tz}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <DataModal
+        open={open}
+        handleClose={handleClose}
+        data={interval}
+        name={name}
+      />
     </div>
   );
 }
 
 export default App;
+const useStyles = makeStyles(theme => ({
+  table: {
+    width: 800,
+    marginTop: "30vh",
+  },
+  tableContent: {
+    fontWeight: "bold",
+    fontSize: "20px",
+    color: "#465362",
+  },
+  TableContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    outline: "none",
+    borderRadius: 3,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(9, 9, 7),
+  },
+}));
---
+++ src/DataModal.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import TextField from "@material-ui/core/TextField";
+import moment from "moment";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+const DataModal = ({ open, handleClose, data, name }) => {
+  const classes = useStyles();
+  const [state, setState] = useState([]);
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+  const [activityData, setActivityData] = useState([]);
+  const [log, setLog] = useState(false);
+
+  useEffect(() => {
+    setState(data);
+    setLog(false);
+    setDate(moment().format("YYYY-MM-DD"));
+  }, [data]);
+
+  const handleChange = e => {
+    const currentDate = e.target.value;
+    setDate(currentDate);
+    let logJudge = false;
+    const extra = [];
+    state.forEach((r, i) => {
+      if (currentDate === r.start_date) {
+        console.log(true);
+        const start = r.start_time;
+        const end = r.end_time;
+        const obj = [{ s: start, e: end }];
+        extra.push(obj);
+        setActivityData(obj);
+        logJudge = true;
+      }
+    });
+
+    setLog(logJudge);
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <Typography variant="h6" gutterBottom>
+              {name}
+            </Typography>
+            <Divider light />
+            <div className={classes.container} noValidate>
+              <TextField
+                id="date"
+                label="Activit-Day"
+                type="date"
+                value={date}
+                onChange={handleChange}
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              {log ? (
+                <div className={classes.timelog}>
+                  {activityData.map((r, i) => (
+                    <div className={classes.activity} key={i + "ka"}>
+                      <Typography variant="body1" gutterBottom>
+                        Start_Time :{r.s}
+                      </Typography>
+                      <Typography variant="body1" gutterBottom>
+                        End_Time :{r.e}
+                      </Typography>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className={classes.activity}>
+                  <Typography variant="body1" gutterBottom>
+                    OOPS.... no work
+                  </Typography>
+                </div>
+              )}
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default DataModal;
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    outline: "none",
+    padding: theme.spacing(1, 3, 2),
+  },
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    padding: "4px",
+    justifyContent: "space-between",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+    margin: "5px",
+  },
+  timelog: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  activity: {
+    marginLeft: "35px",
+    width: 200,
+    margin: "5px",
+  },
+}));
